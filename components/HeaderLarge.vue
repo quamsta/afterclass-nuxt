dbfd872fd884ee99c0c9a08df63ab46712b2bd54
Dustin Quam <quamsta@gmail.com>
2022-10-24 13:16:52 -0500

    fix for cached tab

--- components/HeaderLarge.vue
+++ components/HeaderLarge.vue
@@ -4,22 +4,22 @@
       <div class="row d-none d-lg-block">
         <div class="col-lg-12 content-container title__container">
           <div class="header-graphic">
-            <a href="/" class="d-block">
+            <NuxtLink to="/" class="d-block">
               <img
                 class="header-graphic__img"
                 src="~/assets/images/header.png"
                 alt="After Class: UI Culture, arts, and entertainment."
               />
-            </a>
+            </NuxtLink>
           </div>
         </div>
       </div>
       <header role="banner">
         <!--Navbar-->
         <nav class="navbar navbar-main navbar-expand-lg navbar-dark">
-          <a class="navbar-brand d-lg-none" href="/" aria-label="Home">
+          <NuxtLink class="navbar-brand d-lg-none" href="/" aria-label="Home">
             <img class="brand-image" src="~/assets/images/brand-full.png" />
-          </a>
+          </NuxtLink>
           <button
             class="navbar-toggler border-0"
             type="button"
@@ -39,8 +39,9 @@
             <!-- Links -->
             <ul class="navbar-nav">
               <li class="nav-item mr-lg-4">
-                <a class="nav-link nav-link--large" href="events/"
-                  ><i aria-hidden="true" class="fa fa-calendar"></i> Calendar</a
+                <NuxtLink class="nav-link nav-link--large" to="/"
+                  ><i aria-hidden="true" class="fa fa-calendar"></i>
+                  Calendar</NuxtLink
                 >
               </li>
               <li class="nav-item mr-lg-3">
@@ -87,7 +88,4 @@
   </div>
 </template>
 <style scoped>
-
-
-
 </style>

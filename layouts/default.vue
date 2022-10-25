c72fc35942597cd0080fb11580dffa6cf94836c1
Dustin Quam <quamsta@gmail.com>
2022-10-25 17:14:54 -0500

    removed thing for now

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,19 +5,7 @@
 </template>
 
 <script>
-export default {
 
-    watch: {
-        $route(to, from) {
-            var menuCollapse = document.getElementById('navbarSupportedContent')
-            var bsCollapse = new bootstrap.Collapse(menuCollapse, {
-                toggle: false
-            })
-            bsCollapse.hide()
-        },
-    },
-
-}
 </script>
 <style lang="scss">
 @import "~/assets/scss/main.scss";

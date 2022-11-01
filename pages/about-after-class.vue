50a7b87bfe7b44c067f561d10c09b195044a84a9
Dustin Quam <quamsta@gmail.com>
2022-11-01 13:52:47 -0500

    fixed some broken links

--- pages/about-after-class.vue
+++ pages/about-after-class.vue
@@ -1,46 +1,68 @@
 <template>
-    <div>
-        <Title>About After Class</Title>
-        <HeaderSmall />
+  <div>
+    <Title>About After Class</Title>
+    <HeaderSmall />
 
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-sm col-lg-9 col-xl-8 content-container pt-5" role="main">
-                    <article>
-                        <h1>About After Class</h1>
-                        <div class="content">
-                            <p>A project of the <a href="http://studentlife.uiowa.edu">Division of Student Life</a> at
-                                the University of Iowa, After Class is curated from events found at <a rel="noopener"
-                                    href="http://events.uiowa.edu" target="_blank">events.uiowa.edu</a>.</p>
-                            <p>Use After Class to promote your event to a student audience! You can add your own events
-                                using the button below.&nbsp;</p>
-                            <h2>Contact us/report a problem</h2>
-                            <p>After Class is maintained by <a rel="noopener" href="https://slc.studentlife.uiowa.edu"
-                                    target="_blank">Student Life Communications</a>.</p>
-                            <p>Contact us by emailing <a href="mailto:dsl-web@uiowa.edu">dsl-web@uiowa.edu</a>.</p>
-                            <h2>Add your event</h2>
-                            <p>Submit your event and we'll post it! We might edit your event details, or contact you for
-                                more information. Keep it G-rated (or at least college student-rated) folks.</p>
-                            <p><a class="btn btn-primary btn-large" href="add/">Add Your Event</a></p>
-                        </div>
-
-
-                    </article>
-
-                </div>
-
-
-
+    <div class="container">
+      <div class="row justify-content-center">
+        <div
+          class="col-sm col-lg-9 col-xl-8 content-container pt-5"
+          role="main"
+        >
+          <article>
+            <h1>About After Class</h1>
+            <div class="content">
+              <p>
+                A project of the
+                <a href="http://studentlife.uiowa.edu"
+                  >Division of Student Life</a
+                >
+                at the University of Iowa, After Class is curated from events
+                found at
+                <a rel="noopener" href="http://events.uiowa.edu" target="_blank"
+                  >events.uiowa.edu</a
+                >.
+              </p>
+              <p>
+                Use After Class to promote your event to a student audience! You
+                can add your own events using the button below.&nbsp;
+              </p>
+              <h2>Contact us/report a problem</h2>
+              <p>
+                After Class is maintained by
+                <a
+                  rel="noopener"
+                  href="https://slc.studentlife.uiowa.edu"
+                  target="_blank"
+                  >Student Life Communications</a
+                >.
+              </p>
+              <p>
+                Contact us by emailing
+                <a href="mailto:dsl-web@uiowa.edu">dsl-web@uiowa.edu</a>.
+              </p>
+              <h2>Add your event</h2>
+              <p>
+                Submit your event and we'll post it! We might edit your event
+                details, or contact you for more information. Keep it G-rated
+                (or at least college student-rated) folks.
+              </p>
+              <p>
+                <a
+                  class="btn btn-primary btn-outline-white btn-large"
+                  href="/add/"
+                  >Add Your Event</a
+                >
+              </p>
             </div>
+          </article>
         </div>
-
-
+      </div>
     </div>
+  </div>
 </template>
 <script setup>
-
 </script>
   
 <style scoped>
-
 </style>

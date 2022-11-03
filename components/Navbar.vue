26e5f022eee6b3eedf64a49d89e1bf890fc60260
Dustin Quam <quamsta@gmail.com>
2022-11-03 12:23:54 -0500

    update navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -27,8 +27,8 @@
         <ul class="navbar-nav">
           <li class="nav-item me-4">
             <NuxtLink class="nav-link nav-link--large" to="/">
-              <font-awesome-icon icon="fa-solid fa-calendar" />
-              Calendar
+              <font-awesome-icon icon="fa-solid fa-calendar" class="me-1" />
+              <span>Calendar</span>
             </NuxtLink>
           </li>
           <li class="nav-item me-3">
@@ -36,18 +36,23 @@
               class="nav-link nav-link--large"
               href="/events/interest/7491"
             >
-              <font-awesome-icon :icon="['fas', 'moon']" /> Late Night
+              <font-awesome-icon :icon="['fas', 'moon']" class="me-1" />
+              <span>Late Night</span>
             </NuxtLink>
           </li>
           <li class="nav-item me-3">
             <NuxtLink class="nav-link nav-link--large" href="/events/today">
-              <font-awesome-icon icon="fa-solid fa-clock" /> Today
+              <font-awesome-icon icon="fa-solid fa-clock" class="me-1" />
+              <span>Today</span>
             </NuxtLink>
           </li>
           <li class="nav-item me-3">
             <NuxtLink class="nav-link nav-link--large" href="/events/weekend/">
-              <font-awesome-icon icon="fa-solid fa-calendar-week" />
-              Weekend
+              <font-awesome-icon
+                icon="fa-solid fa-calendar-week"
+                class="me-1"
+              />
+              <span>Weekend</span>
             </NuxtLink>
           </li>
 
@@ -57,8 +62,8 @@
               href="/add/"
               title="Add Your Event"
             >
-              <font-awesome-icon icon="fa-solid fa-plus-circle" />
-              Add Your Event
+              <font-awesome-icon icon="fa-solid fa-plus-circle" class="me-1" />
+              <span>Add Your Event</span>
             </NuxtLink>
           </li>
           <li class="nav-item me-3 link nav-item">
@@ -67,7 +72,8 @@
               href="/about-after-class/"
               title="About After Class"
             >
-              <font-awesome-icon icon="fa-solid fa-info-circle" /> About
+              <font-awesome-icon icon="fa-solid fa-info-circle" class="me-1" />
+              <span>About</span>
             </NuxtLink>
           </li>
         </ul>
